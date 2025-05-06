ab52adadd7c0351bd14f9466d83d289539a19901
manankumar94 <mortal9193@gmail.com>
2025-05-07 04:41:01 +0530

    main file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import  connectDB from "./config/db.js";
+import router from "./routes/routes.js";
 
 dotenv.config();
 
@@ -9,9 +11,13 @@ const app= express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api", router);
 
-
-app.listen(PORT, (err)=>{
-    if(err) console.log(err);
-    else console.log(`Server is running on ${PORT}..`);
+connectDB().then( ()=>{
+    app.listen(PORT, (err)=>{
+        if(err) console.log(err);
+        else console.log(`Server is running on ${PORT}..`);
+    })
 })
+
+
